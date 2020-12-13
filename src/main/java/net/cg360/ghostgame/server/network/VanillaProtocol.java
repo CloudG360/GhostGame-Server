210e33a200a192c7836968fc2147b83fdcb36347
CG360 <CloudGamer360@users.noreply.github.com>
2020-12-13 15:09:47 +0000

    Shuffle packets around and add detail packets to protocol list.

--- src/main/java/net/cg360/ghostgame/server/network/VanillaProtocol.java
+++ src/main/java/net/cg360/ghostgame/server/network/VanillaProtocol.java
@@ -7,21 +7,32 @@ public class VanillaProtocol {
 
     // -- Packet Identifiers --
 
-    //Protocol packets 0x0[<2..] - These should not change in format, even after a large update for consistency.
-    public static final byte PACKET_PROTOCOL_CHECK = 0x00;
-    public static final byte PACKET_PROTOCOL_SUCCESS = 0x01;
-    public static final byte PACKET_PROTOCOL_ERROR = 0x02;
+    //Protocol packets 0x0[0-7] - These should not change in format, even after a large update for consistency.
+    public static final byte PACKET_PROTOCOL_SERVER_DETAIL = 0x00; // Like a ping, should only be extended. Includes stuff like name + logo if present.
+    public static final byte PACKET_PROTOCOL_CLIENT_DETAIL = 0x01; // Informs based on OS, version, and other details.
+    public static final byte PACKET_PROTOCOL_CHECK = 0x02; //in. Can be extended with new data. Includes vital protocol info. Nothing should be removed to accomodate for older clients.
+    public static final byte PACKET_PROTOCOL_SUCCESS = 0x03;
+    public static final byte PACKET_PROTOCOL_ERROR = 0x04;
 
-    //Generic Packets 0x0[3+..]
-    public static final byte PACKET_SERVER_NOTICE = 0x06;
-    public static final byte PACKET_RESPONSE_WARNING = 0x07;
-    public static final byte PACKET_RESPONSE_SUCCESS = 0x08; //out
-    public static final byte PACKET_RESPONSE_ERROR = 0x09; //out
+
+    //Generic Packets 0x0[8-f]
+    public static final byte PACKET_SERVER_NOTICE = 0x08; //out
+    public static final byte PACKET_RESPONSE_WARNING = 0x07; //out
+    public static final byte PACKET_RESPONSE_SUCCESS = 0x09; //out
+    public static final byte PACKET_RESPONSE_ERROR = 0x0a; //out
+    public static final byte PACKET_DISCONNECT_REASON = 0x0b; //Use before closing connection.
 
     //Account Management Packets 0x1[...]
     public static final byte PACKET_LOGIN = 0x10; //in
-    public static final byte PACKET_TOKEN = 0x11; //out
+    public static final byte PACKET_LOGIN_RESPONSE = 0x11; //out - Could split into token packet + error packet.
 
     public static final byte PACKET_CREATE_ACCOUNT = 0x13; //in
+
+    // Session Stuff
+    public static final byte PACKET_SESSION_JOIN_REQUEST = 0x30;
+    public static final byte PACKET_SESSION_CREATE_REQUEST = 0x31;
+
+
+
 
 }
