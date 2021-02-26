ec2df58f35abc6af222139120648c1a8958adf43
Will <willscullythewag@gmail.com>
2021-02-26 14:26:21 +0000

    Added constants

---
+++ src/main/java/net/cg360/spookums/server/util/Constants.java
@@ -0,0 +1,11 @@
+package net.cg360.spookums.server.util;
+
+/**
+ * Some constants that may be used across multiple projects.
+ * @author CG360
+ */
+public class Constants {
+
+    public static final String COMMON_PREFIX = "spooks";
+
+}
