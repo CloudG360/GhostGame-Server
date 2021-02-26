2a59928c8c60a92c2f875e957a4a1f7dc2ff7e55
Will <willscullythewag@gmail.com>
2021-02-26 14:24:01 +0000

    Add immutable utility

---
+++ src/main/java/net/cg360/spookums/server/util/Immutable.java
@@ -0,0 +1,22 @@
+package net.cg360.spookums.server.util;
+
+import java.nio.ByteBuffer;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class Immutable {
+
+    public static <K, V> Map<K, V> uMap(Map<K, V> obj, boolean isUnmodifiable){
+        return isUnmodifiable ? Collections.unmodifiableMap(obj) : obj;
+    }
+
+    public static <V> List<V> uList(List<V> obj, boolean isUnmodifiable){
+        return isUnmodifiable ? Collections.unmodifiableList(obj) : obj;
+    }
+
+    public static ByteBuffer uBuffer(ByteBuffer obj, boolean isUnmodifiable){
+        return isUnmodifiable ? ((ByteBuffer) obj.clear()).asReadOnlyBuffer() : obj;
+    }
+
+}
