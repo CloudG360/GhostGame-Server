4a77759e0d4c55f0379f8e0915354021897fe7fa
Will <willscullythewag@gmail.com>
2021-03-17 22:30:37 +0000

    add data path to server main class

--- src/main/java/net/cg360/spookums/server/Server.java
+++ src/main/java/net/cg360/spookums/server/Server.java
@@ -2,15 +2,28 @@ package net.cg360.spookums.server;
 
 import net.cg360.spookums.server.util.data.Settings;
 
+import java.io.File;
+
 public class Server {
 
     private static Server instance;
 
     private Settings settings;
+    private File dataPath;
 
-    public Server(String[] launchArguments){
+    public Server(String[] args){
+        dataPath = new File("./"); // Run from where jar is ran from. Maybe make it configurable?
+    }
+
+
+
+    public void prepareDatabases() {
 
     }
+
+
+    public Settings getSettings() { return settings; }
+    public File getDataPath() { return dataPath; }
 
     public static Server get() { return instance; }
 
