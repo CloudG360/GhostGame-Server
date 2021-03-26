0b52f455ee6bee073cd5461921caf7f79d50c08b
Will <16578982+CloudG360@users.noreply.github.com>
2021-03-26 13:41:14 +0000

    Implement NodeBlueprint

---
+++ src/main/java/net/cg360/spookums/server/entity/behaviour/types/NodeBlueprint.java
@@ -0,0 +1,31 @@
+package net.cg360.spookums.server.entity.behaviour.types;
+
+import net.cg360.spookums.server.util.Check;
+import net.cg360.spookums.server.util.data.LockableSettings;
+import net.cg360.spookums.server.util.data.Settings;
+import net.cg360.spookums.server.util.data.keyvalue.Key;
+import net.cg360.spookums.server.util.id.Identifier;
+
+public class NodeBlueprint<T extends NodeBaseBehaviour> {
+
+    public Identifier identifier;
+    public Class<T> nodeClass;
+    public Settings defaultSettings;
+
+    public NodeBlueprint(Identifier identifier, Class<T> nodeClass, Settings defaultSettings) {
+        Check.nullParam(identifier, "identifier");
+        Check.nullParam(nodeClass, "nodeClass");
+
+        this.identifier = identifier;
+        this.nodeClass = nodeClass;
+
+        this.defaultSettings = new LockableSettings(defaultSettings == null ? new Settings() : defaultSettings, true);
+    }
+
+
+
+    public Key<T> getKey() { return new Key<>(getIdentifier().getID()); }
+    public Identifier getIdentifier() { return identifier; }
+    public Class<T> getNodeClass() { return nodeClass; }
+    public Settings getDefaultSettings() { return defaultSettings; }
+}
