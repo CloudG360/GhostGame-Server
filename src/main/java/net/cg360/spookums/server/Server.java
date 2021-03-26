21136494e4161d215ca96a275e3834424b1627cb
Will <16578982+CloudG360@users.noreply.github.com>
2021-03-26 13:56:48 +0000

    Force cmd output in environments that support it

--- src/main/java/net/cg360/spookums/server/Server.java
+++ src/main/java/net/cg360/spookums/server/Server.java
@@ -2,7 +2,10 @@ package net.cg360.spookums.server;
 
 import net.cg360.spookums.server.util.data.Settings;
 
+import java.awt.*;
+import java.io.Console;
 import java.io.File;
+import java.io.IOException;
 
 public class Server {
 
@@ -27,10 +30,22 @@ public class Server {
 
     public static Server get() { return instance; }
 
+
+
     /**
      * Launches the server from the jar.
      * @param args arguments entered when running the jar.
      */
-    public static void main(String[] args){ instance = new Server(args); }
+    public static void main(String[] args) throws IOException {
+
+        if((System.console() == null) && (!GraphicsEnvironment.isHeadless())) { // No console window exists but one is supported.
+            String filename = Server.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6);
+            Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""}); // Run the jar but in a cmd window.
+
+        } else {
+            instance = new Server(args);
+            System.out.println("!!!  Stopped Server :^)  !!!"); // No logger prepared, use java's own methods (ew)
+        }
+    }
 
 }
