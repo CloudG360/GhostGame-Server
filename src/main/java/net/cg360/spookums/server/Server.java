72a7b35c0853e738b09c5b27d51546d0380e73ba
Will <16578982+CloudG360@users.noreply.github.com>
2021-03-26 14:03:00 +0000

    Ensure program can run in intellij with a -headless mode :^)

--- src/main/java/net/cg360/spookums/server/Server.java
+++ src/main/java/net/cg360/spookums/server/Server.java
@@ -6,6 +6,9 @@ import java.awt.*;
 import java.io.Console;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Server {
 
@@ -16,6 +19,9 @@ public class Server {
 
     public Server(String[] args){
         dataPath = new File("./"); // Run from where jar is ran from. Maybe make it configurable?
+        while (true) {
+            System.out.println("AAA");
+        }
     }
 
 
@@ -37,8 +43,13 @@ public class Server {
      * @param args arguments entered when running the jar.
      */
     public static void main(String[] args) throws IOException {
+        List<String> argsList = Arrays.asList(args);
 
-        if((System.console() == null) && (!GraphicsEnvironment.isHeadless())) { // No console window exists but one is supported.
+        // Checks:
+        // - Is no terminal window present?
+        // - Does the user environment support a graphical terminal?
+        // - Headless flag *is not* present.
+        if((System.console() == null) && (!GraphicsEnvironment.isHeadless()) && (!argsList.contains("-headless"))) {
             String filename = Server.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6);
             Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""}); // Run the jar but in a cmd window.
 
