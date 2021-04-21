376a7680269c787b8b61828031a72c53b470bbc5
Will <16578982+CloudG360@users.noreply.github.com>
2021-04-21 09:51:03 +0100

    Update VanillaProtocol documentation

--- src/main/java/net/cg360/spookums/server/network/VanillaProtocol.java
+++ src/main/java/net/cg360/spookums/server/network/VanillaProtocol.java
@@ -9,36 +9,38 @@ public class VanillaProtocol {
 
     // -- Packet Identifiers --
 
-    //Protocol packets 0x0? - These should not change in format, even after a large update for consistency.
-    public static final byte PACKET_PROTOCOL_CHECK = 0x01; //in. Can be appened with new data. Includes vital protocol info. Nothing should be removed to accomodate for older clients.
-    public static final byte PACKET_PROTOCOL_SUCCESS = 0x02;
+    // Protocol packets - These should not change in format, even after a large update for consistency.
+    public static final byte PACKET_PROTOCOL_CHECK = 0x01; // in - Can be appened with new data. Includes vital protocol info. Nothing should be removed/reordered to accommodate for older clients.
+    public static final byte PACKET_PROTOCOL_SUCCESS = 0x02; // out - confirms the client is compatible.
     public static final byte PACKET_PROTOCOL_ERROR = 0x03;
-    public static final byte PACKET_PROTOCOL_BATCH = 0x04;
+    public static final byte PACKET_PROTOCOL_BATCH = 0x04; // Unused currently. Probably a good idea though.
 
 
-    //Infomation Packets 0x1+
-    public static final byte PACKET_SERVER_DETAIL = 0x10; // Like a ping, should only be extended. Includes stuff like name + logo if present.
-    public static final byte PACKET_CLIENT_DETAIL = 0x11; // Informs based on OS, version, and other details.
+    // Information Packets
+    public static final byte PACKET_SERVER_PING_REQUEST = 0x11; // in - responded to with PACKET_SERVER_DETAIL
+    public static final byte PACKET_SERVER_DETAIL = 0x11; // out - JSON format - Like a ping, should only be extended. Includes stuff like name + logo if present.
+    public static final byte PACKET_CLIENT_DETAIL = 0x12; // in - JSON format - Stores client OS, version, and other non-essential details. Could be use to split platforms
 
-    //Generic Packets
-    public static final byte PACKET_SERVER_NOTICE = 0x12; //out
-    public static final byte PACKET_RESPONSE_WARNING = 0x13; //out
-    public static final byte PACKET_RESPONSE_SUCCESS = 0x14; //out
-    public static final byte PACKET_RESPONSE_ERROR = 0x15; //out
-    public static final byte PACKET_DISCONNECT_REASON = 0x16; //Use before closing connection.
+    // Generic Packets
+    public static final byte PACKET_SERVER_NOTICE = 0x12; // out - Used to display generic information to a user
+    public static final byte PACKET_RESPONSE_WARNING = 0x13; // out - Used to respond to client packets with a warn status
+    public static final byte PACKET_RESPONSE_SUCCESS = 0x14; // out - Used to respond to client packets with a info status
+    public static final byte PACKET_RESPONSE_ERROR = 0x15; // out - Used to respond to client packets with a error status
+    public static final byte PACKET_DISCONNECT_REASON = 0x16; // in/out - Sent by the server/client that's closing the connection.
 
-    //Account Management Packets
-    public static final byte PACKET_LOGIN = 0x20; //in
-    public static final byte PACKET_LOGIN_RESPONSE = 0x21; //out - Could split into token packet + error packet.
-    public static final byte PACKET_CREATE_ACCOUNT = 0x22; //in
+    // Account Management Packets
+    public static final byte PACKET_LOGIN = 0x20; // in - User attempts to login to their account
+    public static final byte PACKET_CREATE_ACCOUNT = 0x22; // in - User attempts to create an account (Returns a login response packet)
+    public static final byte PACKET_LOGIN_RESPONSE = 0x24; // out - Could split into token (success) packet + error (failure) packet.
+
 
     // Session Stuff
-    public static final byte PACKET_SESSION_JOIN_REQUEST = 0x30;
-    public static final byte PACKET_SESSION_CREATE_REQUEST = 0x31;
+    public static final byte PACKET_GAME_JOIN_REQUEST = 0x30; // in - Client's intent to join a game. Will return a PACKET_SESSION_RESPONSE type packet
+    public static final byte PACKET_GAME_CREATE_REQUEST = 0x31; // in - Client's intent to create their own game with it's settings included.
+    public static final byte PACKET_GAME_RESPONSE = 0x32; // out - Returns a specific game token or an error message
 
-    //
-
-
-
+    public static final byte PACKET_FETCH_GAME_LIST = 0x33; // in - Requests a list of games (Responded to with a few PACKET_GAME_DETAIL's)
+    public static final byte PACKET_REQUEST_GAME_DETAIL = 0x34; // in - Requests the details of a specific game
+    public static final byte PACKET_GAME_DETAIL = 0x35; // out - Sends details of the game to the client
 
 }
