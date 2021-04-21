bd03ebc7294bd8deb768dab3d1fe413b5da44099
Will <16578982+CloudG360@users.noreply.github.com>
2021-04-21 10:20:57 +0100

    Implemented PacketRegistry

---
+++ src/main/java/net/cg360/spookums/server/network/PacketRegistry.java
@@ -0,0 +1,50 @@
+package net.cg360.spookums.server.network;
+
+import net.cg360.spookums.server.network.packet.NetworkPacket;
+import net.cg360.spookums.server.util.Check;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+// While it's not essential register a packet here, only packets
+// found here are processed when recieved by the server.
+public class PacketRegistry {
+
+    private static PacketRegistry primaryInstance = null;
+
+    protected HashMap<Byte, Class<? extends NetworkPacket>> packetTypes;
+
+    public PacketRegistry() {
+        this.packetTypes = new HashMap<>();
+    }
+
+    public boolean setAsPrimaryInstance() {
+        if(primaryInstance == null) {
+            primaryInstance = this;
+            return true;
+        }
+        return false;
+    }
+
+
+
+    public Optional<Class<? extends NetworkPacket>> getPacketTypeForID(byte id) {
+        return Optional.ofNullable(this.packetTypes.get(id));
+    }
+
+    public boolean registerPacketType(byte id, Class<? extends NetworkPacket> type) {
+        Check.nullParam(type, "type");
+
+        if(!this.packetTypes.containsKey(id)) {
+            this.packetTypes.put(id, type);
+            return true;
+        }
+        return false;
+    }
+
+
+
+    public static PacketRegistry get() {
+        return primaryInstance;
+    }
+}
