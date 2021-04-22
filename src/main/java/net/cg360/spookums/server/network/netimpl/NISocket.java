0bfa5d1c5119f614409b129fb602e905e4bd5f7f
Will <16578982+CloudG360@users.noreply.github.com>
2021-04-23 00:37:39 +0100

    Implemented #sendDataPacket() and #broadcastDataPacket() for NISocket

--- src/main/java/net/cg360/spookums/server/network/netimpl/NISocket.java
+++ src/main/java/net/cg360/spookums/server/network/netimpl/NISocket.java
@@ -5,7 +5,10 @@ import net.cg360.spookums.server.network.VanillaProtocol;
 import net.cg360.spookums.server.network.packet.NetworkPacket;
 import net.cg360.spookums.server.network.packet.generic.PacketDisconnect;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.*;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
@@ -67,23 +70,42 @@ public class NISocket implements NetworkInterface {
     }
 
     @Override
-    public ArrayList<NetworkPacket> checkForInboundPackets(UUID user) {
+    public synchronized ArrayList<NetworkPacket> checkForInboundPackets(UUID user) {
         return null;
     }
 
     @Override
-    public HashMap<UUID, ArrayList<NetworkPacket>> checkForInboundPackets() {
+    public synchronized HashMap<UUID, ArrayList<NetworkPacket>> checkForInboundPackets() {
         return null;
     }
 
     @Override
     public synchronized void sendDataPacket(UUID clientNetID, NetworkPacket packet, boolean isUrgent) {
 
+        if(isClientConnected(clientNetID)) {
+            Socket client = clientSockets.get(clientNetID);
+            ByteBuffer content = packet.encode();
+            content.clear();
+
+            byte[] contents = new byte[content.capacity()];
+            content.get(contents);
+
+            try {
+                OutputStream outputStream = client.getOutputStream();
+                outputStream.write(contents);
+
+            } catch (SocketException err) {
+                err.printStackTrace();
+
+            } catch (IOException ioErr) {
+                throw new RuntimeException("An IOException was raised whilst sending a packet: "+ioErr.getMessage());
+            }
+        }
     }
 
     @Override
     public synchronized void broadcastDataPacket(NetworkPacket packet, boolean isUrgent) {
-
+        for(UUID uuid: getClientNetIDs()) sendDataPacket(uuid, packet, isUrgent);
     }
 
     @Override
@@ -93,7 +115,9 @@ public class NISocket implements NetworkInterface {
             Socket conn = clientSockets.get(uuid);
 
             if(conn.isConnected() && (!conn.isClosed())) {
-                sendDataPacket(uuid, disconnectPacket, true);
+
+                try { sendDataPacket(uuid, disconnectPacket, true); }
+                catch (RuntimeException err) { Server.getMainLogger().warn("Client disconnected with a IOException"); }
 
                 try { conn.close(); }
                 catch (Exception err) { err.printStackTrace(); }
@@ -102,7 +126,6 @@ public class NISocket implements NetworkInterface {
             //Server.get().getServerEventManager().call(); Call an event to indicate a client has been disconnected.
 
             clientSockets.remove(uuid);
-
         }
     }
 
