0f2b284aea4b8dd6082fc60e1912ece29c14a557
CloudG360 <16578982+CloudG360@users.noreply.github.com>
2021-07-01 13:04:07 +0100

    Add string types and unsigned short getter to NetworkBuffer

--- src/main/java/net/cg360/spookums/server/util/NetworkBuffer.java
+++ src/main/java/net/cg360/spookums/server/util/NetworkBuffer.java
@@ -2,7 +2,14 @@ package net.cg360.spookums.server.util;
 
 import net.cg360.spookums.server.Server;
 
+import java.nio.BufferOverflowException;
+import java.nio.BufferUnderflowException;
+import java.nio.charset.StandardCharsets;
+
 public class NetworkBuffer {
+
+    public static int MAX_UNSIGNED_SHORT_VALUE = (int) (Math.pow(2, 16) - 1);
+    public static short MAX_UNSIGNED_BYTE_VALUE = (short) (Math.pow(2, 8) - 1);
 
     protected byte[] buffer;
     protected int pointerIndex;
@@ -59,13 +66,67 @@ public class NetworkBuffer {
         return b;
     }
 
+    protected byte[] fetchRawBytes(int byteCount) {
+        byte[] bytes = new byte[byteCount];
+
+        for(int i = 0; i < byteCount; i++) {
+            bytes[i] = buffer[pointerIndex];
+            pointerIndex++;
+        }
+        return bytes;
+    }
+
     /** Unsafe way to write a byte. Make sure to check first :)*/
     protected void writeByte(byte b) {
         buffer[pointerIndex] = b;
         pointerIndex++;
     }
 
+    /** Unsafe way to write a series of bytes. Make sure to check first :)*/
+    protected void writeBytes(byte[] bytes) {
+        for(byte b : bytes) writeByte(b);
+    }
 
+    // Could be signed, unsigned, or even a string character.
+    /** Fetches a byte from the buffer without converting it.*/
+    public byte get() {
+        if(canReadBytesAhead(1)) {
+            return fetchRawByte();
+        }
+        throw new BufferUnderflowException();
+    }
+
+    public int getUnsignedShort() {
+        if(canReadBytesAhead(2)) {
+            int total = 0;
+
+            total += (((int) fetchRawByte()) << 8) & 0xFF00;
+            total += ((int) fetchRawByte()) & 0x00FF;
+
+            return total & 0xFFFF;
+        }
+        throw new BufferUnderflowException();
+    }
+
+    public String getUnboundUTF8String(int byteCount) {
+        if(canReadBytesAhead(byteCount)) {
+           byte[] strBytes = fetchRawBytes(byteCount);
+           return new String(strBytes, StandardCharsets.UTF_8);
+        }
+        throw new BufferUnderflowException();
+    }
+
+
+
+
+    /** Sets bytes to the buffer without converting it.*/
+    public boolean put(byte... b) {
+        if(canReadBytesAhead(b.length)) {
+            for (byte value : b) writeByte(value);
+            return true;
+        }
+        return false;
+    }
 
     public boolean putUnsignedByte(int value) {
         if(value > 255) throw new IllegalArgumentException("Provided an 'unsigned byte' with a value greater than 255");
@@ -117,6 +178,54 @@ public class NetworkBuffer {
         }
 
         return false;
+    }
+
+    /** @return the amount of bytes written. */
+    public int putUTF8String(String string) {
+        if(string.length() == 0) return 0;
+
+        byte[] strBytes = string.getBytes(StandardCharsets.UTF_8);
+        if(strBytes.length <= MAX_UNSIGNED_SHORT_VALUE) throw new IllegalArgumentException("String exceeds the limit of "+MAX_UNSIGNED_SHORT_VALUE+" bytes.");
+
+        // Check if both the length of bytes + the length short can be included.
+        if(canReadBytesAhead(2 + strBytes.length)) {
+            if(putUnsignedShort(strBytes.length)) {
+                writeBytes(strBytes);
+                return 2 + strBytes.length;
+            }
+        }
+
+        return 0;
+    }
+
+    /** @return the amount of bytes written. */
+    public int putSmallUTF8String(String string) {
+        if(string.length() == 0) return 0;
+
+        byte[] strBytes = string.getBytes(StandardCharsets.UTF_8);
+        if(strBytes.length <= MAX_UNSIGNED_BYTE_VALUE) throw new IllegalArgumentException("String exceeds the limit of "+MAX_UNSIGNED_BYTE_VALUE+" bytes.");
+
+        // Check if both the length of bytes + the length short can be included.
+        if(canReadBytesAhead(1 + strBytes.length)) {
+            if(putUnsignedByte(strBytes.length)) {
+                writeBytes(strBytes);
+                return 1 + strBytes.length;
+            }
+        }
+        return 0;
+    }
+
+    /** A string is added without length marking bytes at the start. */
+    public int putUnboundUTF8String(String string) {
+        if(string.length() == 0) return 0;
+        byte[] strBytes = string.getBytes(StandardCharsets.UTF_8);
+
+        // Check if both the length of bytes + the length short can be included.
+        if(canReadBytesAhead(strBytes.length)) {
+            writeBytes(strBytes);
+            return strBytes.length;
+        }
+        return 0;
     }
 
 }
