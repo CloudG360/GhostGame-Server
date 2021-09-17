9a99f865511afa733a0b608e0048b04db6c9a452
CloudG360 <16578982+CloudG360@users.noreply.github.com>
2021-09-17 15:42:56 +0100

    Implemented MicroBoolean for storing flags

---
+++ src/main/java/net/cg360/spookums/server/util/MicroBoolean.java
@@ -0,0 +1,40 @@
+package net.cg360.spookums.server.util;
+
+import net.cg360.spookums.server.util.clean.Check;
+
+public final class MicroBoolean {
+
+    private byte values;
+
+    public MicroBoolean(boolean... bools){
+        this.values = 0x00;
+
+        for(int i = 0; (i < 8) && (i < bools.length); i++) {
+            if(bools[i]) {
+                this.values |= ((0b00000001 << i) & 0xFF);
+            }
+        }
+    }
+
+    public void setValue(int index, boolean value) {
+        Check.inclusiveBounds(index, 0, 7, "index");
+
+        // Sets the bits differently based on if they're true or false.
+        // If true, use an OR bitwise operator to set the position to 1.
+        // If false, use an AND bitwise operator + the compliment of the mask to set
+        // the position to 0.
+        if(value) this.values |= ((0b00000001 << index) & 0xFF);
+        else      this.values &= ((~(0b00000001 << index)) & 0xFF);
+    }
+
+    public boolean getValue(int index) {
+        Check.inclusiveBounds(index, 0, 7, "index");
+        byte mask = (byte) ((0b00000001 << index) & 0xFF);
+
+        return (values & mask) != 0x00; // Return if bit under mask is NOT 0, meaning the bool is false.
+    }
+
+    public byte getStorageByte() {
+        return values;
+    }
+}
