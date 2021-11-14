2742cd3d553f69855f9af55674197d66989481af
CloudG360 <16578982+CloudG360@users.noreply.github.com>
2021-11-14 01:55:28 +0000

    Handled a bunch of cases related to numbers before their parsing is added.

--- src/main/java/net/cg360/spookums/server/core/data/json/io/parse/ArrayParsingFrame.java
+++ src/main/java/net/cg360/spookums/server/core/data/json/io/parse/ArrayParsingFrame.java
@@ -4,10 +4,26 @@ import net.cg360.spookums.server.Server;
 import net.cg360.spookums.server.core.data.json.Json;
 import net.cg360.spookums.server.core.data.json.JsonArray;
 import net.cg360.spookums.server.core.data.json.JsonObject;
+import net.cg360.spookums.server.core.data.json.io.error.JsonFormatException;
+
+import java.util.ArrayList;
 
 public class ArrayParsingFrame extends ParsingFrame {
 
     protected Json<JsonArray> holdingArray = null;
+
+    protected State currentReaderState;
+    protected ArrayList<Character> foundDigits;
+
+
+    public ArrayParsingFrame() {
+        this.holdingArray = null;
+
+        this.currentReaderState = State.FIRST_VALUE;
+        this.foundDigits = new ArrayList<>();
+    }
+
+
 
     @Override
     public boolean shouldHoldOnToCharacter(char character) {
@@ -16,12 +32,45 @@ public class ArrayParsingFrame extends ParsingFrame {
 
     @Override
     public void processCharacter(char character) {
+        if(character == ' ') return; // All spaces are legal
 
+        State lastState = this.currentReaderState;
+
+        switch (lastState) {
+
+            case FIRST_VALUE:
+            case VALUE: // can be on new line
+                //TODO: Accept numbers - CHANGE TO "VALUE" WHEN FIRST NUMBER FOUND
+                // with numbers, look for [0-9.]
+                // this step can skip the comma stage if the comma ends a number, thus terminating the value check.
+                // the comma stage more handles after strings or after numbers when spaces follow them.
+                break;
+
+            case COMMA: // can be on new line
+                if(character == ',') {
+                    this.currentReaderState = State.VALUE;
+                } else throw new JsonFormatException("Unexpected character - expecting comma after entry "+getErrorLineNumber());
+                break;
+
+        }
     }
 
     @Override
     public void processConstructedInnerFrame(Json<?> frame) {
+        if(frame == null) return; // Comments have no effect. Keeping this check here in case of future changes.
+        State lastState = this.currentReaderState;
 
+        switch (lastState) {
+            case FIRST_VALUE:
+            case VALUE:
+
+                this.holdingArray.getValue().addChild(frame);
+                this.currentReaderState = State.COMMA;
+                break;
+
+            case COMMA:
+                throw new JsonFormatException("Unexpected element - expecting comma "+getErrorLineNumber());
+        }
     }
 
     @Override
@@ -32,7 +81,23 @@ public class ArrayParsingFrame extends ParsingFrame {
     @Override
     public Json<?> terminateFrame() {
         //Server.getMainLogger().info(String.format("[Parser] ']' used to close array frame (depth %s)", jsonIOInstance.getParseFrameStack().getSize()));
-        return holdingArray;
+        if((this.currentReaderState == State.VALUE) && hasFoundNumber()) {
+            parseAndAddCollectedDigits();
+            return holdingArray;
+        }
+
+        if(this.currentReaderState == State.FIRST_VALUE || this.currentReaderState == State.COMMA) return holdingArray;
+        else throw new JsonFormatException("Unexpected ending - the object was closed after an invalid character "+getErrorLineNumber());
+        //TODO: Lock the result
+    }
+
+
+    protected void parseAndAddCollectedDigits() {
+
+    }
+
+    protected boolean hasFoundNumber() {
+        return foundDigits.size() > 0;
     }
 
 
@@ -44,6 +109,16 @@ public class ArrayParsingFrame extends ParsingFrame {
     @Override
     public char getClosingCharacter() {
         return ']';
+    }
+
+
+
+    protected enum State {
+
+        FIRST_VALUE, // can terminate here, otherwise, awaiting_identifier.
+        VALUE, // waiting for identifier (string)
+        COMMA, // looking for comma or termination.
+
     }
 
 }
--- src/main/java/net/cg360/spookums/server/core/data/json/io/parse/ObjectParsingFrame.java
+++ src/main/java/net/cg360/spookums/server/core/data/json/io/parse/ObjectParsingFrame.java
@@ -1,12 +1,10 @@
 package net.cg360.spookums.server.core.data.json.io.parse;
 
-import net.cg360.spookums.server.Server;
 import net.cg360.spookums.server.core.data.json.Json;
 import net.cg360.spookums.server.core.data.json.JsonObject;
-import net.cg360.spookums.server.core.data.json.io.JsonIO;
 import net.cg360.spookums.server.core.data.json.io.error.JsonFormatException;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class ObjectParsingFrame extends ParsingFrame {
 
@@ -15,6 +13,7 @@ public class ObjectParsingFrame extends ParsingFrame {
     protected State currentReaderState;
 
     protected String lastFoundIdentifier;
+    protected ArrayList<Character> foundDigits;
 
 
     public ObjectParsingFrame() {
@@ -23,6 +22,7 @@ public class ObjectParsingFrame extends ParsingFrame {
         this.currentReaderState = State.FIRST_AWAITING_IDENTIFIER;
 
         this.lastFoundIdentifier = null;
+        this.foundDigits = new ArrayList<>();
     }
 
 
@@ -33,7 +33,6 @@ public class ObjectParsingFrame extends ParsingFrame {
 
     @Override
     public void processCharacter(char character) {
-        if(character == ' ') return; // All spaces are legal
 
         State lastState = this.currentReaderState;
 
@@ -41,21 +40,26 @@ public class ObjectParsingFrame extends ParsingFrame {
 
             case FIRST_AWAITING_IDENTIFIER:
             case AWAITING_IDENTIFIER:
+                if(character == ' ') return;
                  throw new JsonFormatException("Unexpected character - expecting quotes for identifier "+getErrorLineNumber());
 
             case COLON: // can be on new line
+                if(character == ' ') return;
                 if(character == ':') {
                     this.currentReaderState = State.VALUE;
+                    this.foundDigits.clear();
                 } else throw new JsonFormatException("Unexpected character - expecting colon after identifier "+getErrorLineNumber());
                 break;
 
             case VALUE: // can be on new line
-                // with numbers, look for [0-9.]
+                //TODO: Accept numbers & update found number
+                // with numbers, look for [0-9] initially but continue with [0-9.]
                 // this step can skip the comma stage if the comma ends a number, thus terminating the value check.
                 // the comma stage more handles after strings or after numbers when spaces follow them.
                 break;
 
             case COMMA: // can be on new line
+                if(character == ' ') return;
                 if(character == ',') {
                     this.currentReaderState = State.AWAITING_IDENTIFIER;
                 } else throw new JsonFormatException("Unexpected character - expecting comma after entry "+getErrorLineNumber());
@@ -81,9 +85,11 @@ public class ObjectParsingFrame extends ParsingFrame {
 
             case COLON:
             case COMMA:
-                throw new JsonFormatException("Unexpected element - expecting divider");
+                throw new JsonFormatException("Unexpected element - expecting divider "+getErrorLineNumber());
 
             case VALUE:
+
+                if(this.hasFoundNumber()) throw new JsonFormatException("Unexpected element - number preceding json element "+getErrorLineNumber());
 
                 this.holdingObject.getValue().addChild(lastFoundIdentifier, frame);
                 this.lastFoundIdentifier = null;
@@ -102,8 +108,22 @@ public class ObjectParsingFrame extends ParsingFrame {
     public Json<?> terminateFrame() {
         //Server.getMainLogger().info(String.format("[Parser] '}' used to close object frame (depth %s)", jsonIOInstance.getParseFrameStack().getSize()));
         if(this.currentReaderState == State.FIRST_AWAITING_IDENTIFIER || this.currentReaderState == State.COMMA) return holdingObject;
-        else throw new JsonFormatException("Unexpected ending - the object was closed after an invalid character");
+        else {
+            if(this.currentReaderState == State.VALUE && hasFoundNumber()) {
+                parseAndAddCollectedDigits();
+                return holdingObject;
+            }
+            throw new JsonFormatException("Unexpected ending - the object was closed after an invalid character "+getErrorLineNumber());
+        }
         //TODO: Lock the result
+    }
+
+    protected void parseAndAddCollectedDigits() {
+
+    }
+
+    protected boolean hasFoundNumber() {
+        return foundDigits.size() > 0;
     }
 
 
